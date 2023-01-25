1315936e6adf28b876fcda9fd18ac2be81803781
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-25 05:15:35 -0800

    added a seperate page to request a certain short url id and pass the correct long url on a seperate page,

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get('/urls', (req, res) => {
 res.render('urls_index', templateUrlVariables)
 });
 
+app.get('/urls/:id', (req, res) => {
+  let templateVarsForUrlIDS = {id : req.params.id, longURL: urlDatabase[req.params.id] }
+  res.render('urls_show', templateVarsForUrlIDS)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
